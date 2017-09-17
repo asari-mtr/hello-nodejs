8a5676ad4bb206bd9dc0b4e6e3450b54072920a1
Asari Mitsuteru <mitsuteru.asari@supership.jp>
2017-09-17 21:54:11 +0900

    change to es2015(use bable)

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,12 @@
 module.exports = {
     entry: './main.js',
     output: {filename: 'bundle.js'},
-    devtool: 'source-map'
+    devtool: 'source-map',
+    module: {
+        loaders: [{
+            test: /\.jsx?$/,
+            exclude: /node_modules/,
+            loader: 'babel-loader'
+        }]
+    }
 }
