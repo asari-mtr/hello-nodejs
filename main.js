121bcb42d4ac7610978fbe6be059988e61c1234d
Asari Mitsuteru <mitsuteru.asari@supership.jp>
2017-09-17 21:16:03 +0900

    use webpack

---
+++ main.js
@@ -0,0 +1,10 @@
+var $ = require("jquery");
+
+$(function(){
+    var $msg = $("#msg");
+    $msg.fadeOut("slow", function() {
+        $msg.text("jQuery")
+        .css("color", "red")
+        .fadeIn("slow");
+    });
+})
