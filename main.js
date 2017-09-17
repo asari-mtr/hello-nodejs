c97fa876ecd78187758eef8192fea538ca9d4e93
Asari Mitsuteru <mitsuteru.asari@supership.jp>
2017-09-17 21:18:45 +0900

    use moment

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
 var $ = require("jquery");
+var moment = require("moment");
 
 $(function(){
     var $msg = $("#msg");
     $msg.fadeOut("slow", function() {
-        $msg.text("jQuery")
+        $msg.text("Webpack " +
+        moment().format("YYYY-MM-DD HH:mm:ss"))
         .css("color", "red")
         .fadeIn("slow");
     });
